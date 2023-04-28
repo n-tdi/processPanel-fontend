164404b0dd2ce1efa8a9ea6bae663c51d61205b8
Ntdi <79174456+n-tdi@users.noreply.github.com>
2023-04-27 21:41:59 -0400

    add delete remove and edit

--- src/components/envpage/ENVList.svelte
+++ src/components/envpage/ENVList.svelte
@@ -2,6 +2,9 @@
     import Popup from "../cards/Popup.svelte";
     import EnvRow from "./ENVRow.svelte";
 
+    let addKey = "";
+    let addValue = "";
+
     let penvEnabled = true;
 
     let penv = [
@@ -24,16 +27,75 @@
         });
         console.log("Editing " + key + " to " + value);
     };
+
+    const add = (key, value) => {
+      if (key !== "" && value !== "") penv = [...penv, {key: key, value: value}];
+      addKey = "";
+      addValue = "";
+    }
 </script>
 
 {#if !penvEnabled}
     <Popup title="Error" body="No Parent Environment set."/>
 {:else}
+    <div class="env-header">
+      <h4 class="key-header">KEY</h4>
+      <h4 class="value-header">VALUE</h4>
+    </div>
     {#each penv as env}
         <EnvRow key={env.key} value={env.value} removeFunction={remove} editFunction={edit} />
     {/each}
+    <div class="addition">
+      <form class="sb" on:submit|preventDefault={() => add(addKey, addValue)}>
+        <input type="text" name="key" id="key" class="key" placeholder="Key" bind:value={addKey}>
+        <input type="text" name="value" id="value" class="value" placeholder="Value" bind:value={addValue}>
+        <button class="hide"></button>
+      </form>
+    </div>
 {/if}
 
-<style>
+<style lang=scss>
+  .env-header {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 0 8px 0;
+  }
 
+  .addition {
+    display: flex;
+    flex-direction: column;
+    padding: 8px 0;
+  }
+
+  .sb {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .key, .value {
+    border: none;
+    background-color: $darkest;
+    color: white;
+    border-left: 1px solid $dark;
+    border-right: 1px solid $dark;
+    padding-left: 4px;
+  }
+
+  .bottom {
+    display: flex;
+    justify-content: center;
+  }
+
+  .add-btn {
+    background-color: $darkest;
+    border: none;
+    color: white;
+    font-size: 1rem;
+    font-weight: 800;
+    cursor: pointer;
+  }
+
+  .hide {
+    display: none;
+  }
 </style>
--- src/components/envpage/ENVRow.svelte
+++ src/components/envpage/ENVRow.svelte
@@ -1,17 +1,33 @@
 <script>
-    export let key;
-    export let value;
-    export let removeFunction;
-    export let editFunction;
+  export let key;
+  export let value;
+  export let removeFunction;
+  export let editFunction;
+    
+  let editing = false;
+
+  const toggleEdit = () => {
+    if (editing) {
+      editFunction(key, value);
+    }
+    editing = !editing;
+  }
 </script>
 
 <div class="container">
-    <p class="key">{key}</p>
-    <div class="info">
-        <button class="info-btn" on:click={() => editFunction(key, "new")}>Edit</button>
-        <button class="info-btn" on:click={() => {removeFunction(key)}}>Remove</button>
-    </div>
-    <p class="value">{value}</p>
+  <p class="key">{key}</p>
+  <div class="info">
+      <button class="info-btn" on:click={() => {removeFunction(key)}}>Remove</button>
+  </div>
+  <div class="value-div">
+    {#if !editing}
+      <p class="value" on:click={toggleEdit}>{value}</p>
+    {:else}
+      <form on:submit|preventDefault={toggleEdit}>
+        <input type="text" on:focusout={toggleEdit} name="edit" id="edit" class="edit" bind:value={value}>
+      </form>
+    {/if}
+  </div>
 </div>
 
 <style lang=scss>
@@ -20,9 +36,10 @@
         justify-content: space-between;
         border-bottom: 1px solid $dark;
         padding: 8px 0;
-
+        transition: all 0.1s ease-in-out;
         &:hover {
             .info {
+                opacity: 100%;
                 display: flex;
                 align-items: center;
                 justify-content: space-around;
@@ -31,12 +48,17 @@
         }
     }
 
-    .key, .value {
+    .key .value {
         font-weight: 400;
     }
 
+    .value {
+      cursor: pointer;
+    }
+
     .info {
-        display: none;
+        opacity: 0%;
+        transition: all 0.2s ease-in-out;
     }
 
     .info-btn {
@@ -44,5 +66,14 @@
         background-color: $darkest;
         color: $white;
         cursor: pointer;
+    }
+
+    .edit {
+      border: none;
+      border-left: 1px solid $dark;
+      border-right: 1px solid $dark;
+      background-color: $darkest;
+      color: $white;
+      padding: 0 6px;
     }
 </style>
